6816343410f8d4b748fc0308704e0c8c6df6ab27
Harold Guzman <Gzmn.h10195@gmail.com>
2025-04-18 13:16:15 +0800

    ups

--- src/templates.js
+++ src/templates.js
@@ -7,52 +7,109 @@ export const tempholder = () => {
             label: 'Full block',
             category: 'Sections',
             media: '<svg viewBox="0 0 24 24"><path d="M21 3H3c-.6 0-1 .4-1 1v6c0 .6.4 1 1 1h18c.6 0 1-.4 1-1V4c0-.6-.4-1-1-1m0 10H3c-.6 0-1 .4-1 1v6c0 .6.4 1 1 1h18c.6 0 1-.4 1-1v-6c0-.6-.4-1-1-1Z"/></svg>',
-            content: `    <section class="container_layout_b">
-        <div class="onelayouts lay_a">
+            content: `       <section id="section_041725_a" class="min-h-[80px] flex fex-row w-full max-[919px]:flex-col">
 
-        </div>
+        <div class="flex-1 min-h-[100vh] flex flex-row justify-center items-center p-[10px] pr-[5px]  max-[919px]:pr-[10px] max-[919px]:pb-[5px]">
+            <div class="w-full min-h-[100vh] bg-base-300 rounded-[15px] p-[10px] flex flex-col relative">
+                <img 
+                class="h-full w-full z-[1] absolute object-cover rounded-[15px] top-0 left-0 border"
+                src="https://images.pexels.com/photos/7144714/pexels-photo-7144714.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" >
+                
+                <div class="px-[20px] mt-[40px] z-[2] relative text-base-300">
+                    <span>MORE OFFERS</span>
+                    <h1 class="text-5xl mt-[10px] font-simibold">There is something elese for you</h1>
+                </div>
 
-        <div class="onelayouts lay_b">
-            <div class="o_centera_o">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia, quis?
+                <a href="#">
+                    <div class="absolute p-[5px] text-base-300 bottom-[6%] left-[3%] max-w-[310px] rounded-[8px] flex flex-row  border z-[999] bg-base-300 max-[362px]:scale-[.8] max-[362px]:left-[1%]">
+                        <div class="flex flex-col p-[5px] text-base-content">
+                            <p class="opacity-[.6] italic line-clamp-1">
+                                Lorem, ipsum.
+                            </p>
+                            <h1 class="text-xl line-clamp-2">
+                                Lorem ipsum dolor sit amet.
+                            </h1>
+                        </div>
+                        <div class="flex flex-col justify-center items-center p-[15px] text-base-content"> 
+                            <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="currentColor"><path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"/></svg>
+                        </div>
+                    </div>
+                </a>
+                
             </div>
         </div>
-        
-        <div class="lay_c"> 
-          <div class="onelayouts blay_a">
-              <div class="items">
-                  <div class="card">
-                    <div class="avatar">
-                      <div class="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
-                        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                      </div>
+
+
+        <div class="flex-1 p-[10px] min-h-[100vh] pl-[5px] flex flex-col gap-[10px] max-[919px]:pl-[10px] max-[919px]:pt-[5px]">
+
+            <div class="w-full flex-1 h-full rounded-[15px] bg-base-200 p-[20px] relative">
+                <img src="https://images.pexels.com/photos/7307562/pexels-photo-7307562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="h-full w-full z-[1] absolute object-cover rounded-[15px] top-0 left-0 border" alt="">
+                <div class="p-[10px] z-[3] flex flex-col absolute top-[0] left-[0] p-[15px] text-base-content/100 h-full w-full">
+                        <div class="flex-1 flex flex-row justify-between items-start">
+                            <div class="flex-3">
+                                <p class="font-bold line-clamp-3 uppercase">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                            <div class="flex-1 flex flex-row justify-end items-center">
+                                <a href="#">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="currentColor"><path d="m216-160-56-56 464-464H360v-80h400v400h-80v-264L216-160Z"/></svg>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="flex-1 flex flex-col justify-end">
+                            <a href="#">
+                                <h1 class="text-3xl font-bold line-clamp-1">
+                                    View our Blog
+                                </h1>
+                            </a>
+                        </div>
+                </div>
+            </div>
+
+            <div class="w-full flex-1 h-full flex flex-row gap-[10px] max-[471px]:flex-col">
+
+                <div class="flex-1  flex flex-col p-[15px] rounded-[15px] bg-fuchsia-300">
+                    <div class="flex-1 flex flex-row justify-between items-start">
+                        <div class="flex-3">
+                            <p class="uppercase text-xs">Lorem ipsum dolor sit amet.</p>
+                        </div>
+                        <div class="flex-1 flex flex-row justify-end items-center">
+                            <a href="#">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="currentColor"><path d="m216-160-56-56 464-464H360v-80h400v400h-80v-264L216-160Z"/></svg>
+                            </a>
+                        </div>
                     </div>
-                    <div class="content">
-                      <h1>Lorem ipsum dolor sit amet.</h1>
-                      <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore illum accusamus enim, rem unde tempore!
-                      </p>
+                    <div class="flex-1 flex flex-col justify-end">
+                        <a href="#">
+                            <h1 class="text-3xl font-bold line-clamp-1">
+                                About us
+                            </h1>
+                        </a>
                     </div>
-                  </div>
-              </div>
-              <div class="items">
-                  <div class="card">
-                    <div class="avatar">
-                      <div class="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
-                        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                      </div>
+                </div>
+                <div class="flex-1 flex flex-col p-[15px] rounded-[15px] bg-green-200">
+                    <div class="flex-1 flex flex-row justify-between items-start">
+                        <div class="flex-3">
+                            <p class="uppercase text-xs">Lorem ipsum dolor sit amet.</p>
+                        </div>
+                        <div class="flex-1 flex flex-row justify-end items-center">
+                            <a href="#">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="currentColor"><path d="m216-160-56-56 464-464H360v-80h400v400h-80v-264L216-160Z"/></svg>
+                            </a>
+                        </div>
                     </div>
-                    <div class="content">
-                      <h1>Lorem ipsum dolor sit amet.</h1>
-                      <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore illum accusamus enim, rem unde tempore!
-                      </p>
+                    <div class="flex-1 flex flex-col justify-end">
+                        <a href="#">
+                            <h1 class="text-3xl font-bold line-clamp-1">
+                                Contact us
+                            </h1>
+                        </a>
                     </div>
-                  </div>
-              </div>
-          </div>
-          <div class="onelayouts blay_b" style="background-image: url(https://images.pexels.com/photos/1662159/pexels-photo-1662159.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1);"></div>
+                </div>
+
+            </div>
+
         </div>
+        
     </section>
 `,
             // Full width block
