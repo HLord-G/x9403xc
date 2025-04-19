0df5cfab5492616b768c41e7c072b75c53c277e0
Harold Guzman <Gzmn.h10195@gmail.com>
2025-04-19 17:21:32 +0800

    g

--- src/templates.js
+++ src/templates.js
@@ -4,10 +4,12 @@ export const tempholder = () => {
     return  [
         {
             id: 'section-1',
-            label: 'Full block',
+            label: 'Section A',
             category: 'Sections',
-            media: '<svg viewBox="0 0 24 24"><path d="M21 3H3c-.6 0-1 .4-1 1v6c0 .6.4 1 1 1h18c.6 0 1-.4 1-1V4c0-.6-.4-1-1-1m0 10H3c-.6 0-1 .4-1 1v6c0 .6.4 1 1 1h18c.6 0 1-.4 1-1v-6c0-.6-.4-1-1-1Z"/></svg>',
-            content: `       <section id="section_041725_a" class="min-h-[80px] flex fex-row w-full max-[919px]:flex-col">
+            media: `<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-union" viewBox="0 0 16 16">
+  <path d="M0 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v2h2a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-2H2a2 2 0 0 1-2-2z"/>
+</svg>`,
+            content: `<section id="section_041725_a" class="min-h-[80px] flex fex-row w-full max-[919px]:flex-col">
 
         <div class="flex-1 min-h-[100vh] flex flex-row justify-center items-center p-[10px] pr-[5px]  max-[919px]:pr-[10px] max-[919px]:pb-[5px]">
             <div class="w-full min-h-[100vh] bg-base-300 rounded-[15px] p-[10px] flex flex-col relative">
@@ -110,60 +112,183 @@ export const tempholder = () => {
 
         </div>
         
-    </section>
-`,
-            // Full width block
-            // full: true
-            attributes: { class: 'custom-class' }
+    </section>`,
+ 
+            // attributes: { class: 'custom-class' },
+                // Disable the block
+            // disable: true,
+            // // Custom onClick callback
+            // onClick(block) {
+            // alert(`Block ${block.getLabel()} disabled`);
+            // }
           },
           {
-            id: 'my-block-section-html',
-            label: 'Section A',
+            id: 'section-3_b',
+            label: 'Section - B',
             category: 'Sections',
-            media: '<svg viewBox="0 0 24 24"><path d="M21 3H3c-.6 0-1 .4-1 1v6c0 .6.4 1 1 1h18c.6 0 1-.4 1-1V4c0-.6-.4-1-1-1m0 10H3c-.6 0-1 .4-1 1v6c0 .6.4 1 1 1h18c.6 0 1-.4 1-1v-6c0-.6-.4-1-1-1Z"/></svg>',
-            // HTML strings will be transformed to Component Definitions
-            content: ` 
-                <section id="section_041725_a" class="min-h-[80px] flex fex-row w-full max-[919px]:flex-col">
-    
-    <div class="flex-1 min-h-[100vh] flex flex-row justify-center items-center p-[10px] pr-[5px]  max-[919px]:pr-[10px] max-[919px]:pb-[5px]">
-        <div class="w-full min-h-[100vh] bg-base-300 rounded-[15px] p-[10px] flex flex-col relative">
-            <img 
-            class="h-full w-full z-[1px] absolute rounded-[15px] top-0 left-0 border"
-            src="https://images.pexels.com/photos/7144714/pexels-photo-7144714.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" >
-            <div class="px-[20px] pt-[20px] z-[2px] relative">
-                <span class="text-base-300">MORE OFFERS</span>
-                <h1 class="text-5xl text-base-300 font-simibold">There is something elese for you</h1>
+            media: `<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-union" viewBox="0 0 16 16">
+  <path d="M0 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v2h2a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-2H2a2 2 0 0 1-2-2z"/>
+</svg>`,
+
+            content: `<section id="sections_041825_b" class="min-h-[100vh] flex flex-col w-full relative p-[7px]">
+            <img src="https://images.unsplash.com/photo-1707305639571-d2c2156ec60d?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="object-cover rounded-[25px] absolute top-0 left-0 w-full h-full" alt="">
+
+
+            <main class="absolute flex flex-col p-[20px] top-0 left-0 w-full h-full">
+                <div class="flex-1 w-full flex flex-row justify-around items-start p-[20px] max-[628px]:flex-col">
+                    <a href="#"><p class="text-base-content/60 text-shadow-2xl font-semibold">Best style</p></a>
+                    <a href="#"><p class="text-base-content/60 text-shadow-2xl font-semibold">Modern Art</p></a>
+                    <a href="#"><p class="text-base-content/60 text-shadow-2xl font-semibold">Best of 2028</p></a>
+                </div>
+                <div class="flex-2 w-full  break-words text-wrap flex flex-col justify-center items-center">
+                    <h1 class="text-7xl text-wrap break-words text-center font-black text-base-300 text-shadow-zinc-500 text-shadow max-[410px]:text-5xl">Francore</h1>
+                </div>
+                <div class="flex-2 w-full flex flex-col justify-center items-center gap-[20px]">
+                    <div class="p-[8px] bg-green-100 text-xl">
+                        <h2 class="font-bold text-center">Modern Business Presentation </h2>
+                    </div>
+                    <p class="text-center max-w-[520px] italic">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione tempore velit illo impedit doloremque. Architecto odit iure sunt exercitationem possimus.
+                    </p>
+                </div>
+            </main>
+
+
+    </section>`, 
+            // attributes: { class: 'custom-class' }
+          },{
+            id: 'section-4_c',
+            label: `Section - C`,
+            category: 'Sections',
+            media: `
+<svg xmlns="http://www.w3.org/2000/svg" height="45" viewBox="0 -960 960 960" width="45" fill="currentColor"><path d="M200-520q-33 0-56.5-23.5T120-600v-160q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v160q0 33-23.5 56.5T760-520H200Zm0-80h560v-160H200v160Zm0 480q-33 0-56.5-23.5T120-200v-160q0-33 23.5-56.5T200-440h560q33 0 56.5 23.5T840-360v160q0 33-23.5 56.5T760-120H200Zm0-80h560v-160H200v160Zm0-400v-160 160Zm0 400v-160 160Z"/></svg>`,
+            content: `<section id="sections_041825_c" class="flex flex-row min-h-screen relative max-[769px]:flex-col">
+        <div class="flex-1 flex flex-col min-h-screen">
+            <div class="flex-2 relative  text-wrap break-words p-[20px] flex flex-col  justify-center items-center bg-blue-900">
+                <img class="object-cover z-[1] absolute top-0 left-0 h-full w-full opacity-[.5]"  src="https://images.pexels.com/photos/2422280/pexels-photo-2422280.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                  <h1 class="relative z-[2] text-5xl text-wrap break-words font-bold text-base-300"> Lorem ipsum dolor sit amet. </h1>
             </div>
-            <div>
-                SDFSEFsdf
+            <div class="flex-1 font-bold  flex flex-col p-[20px] justify-center items-center bg-[#8786d4]"> 
+                <h1 class="text-center text-3xl text-base-300  line-clamp-2">Lorem, ipsum dolor.</h1>
             </div>
         </div>
-    </div>
-    
-    
-    <div class="flex-1 p-[10px] min-h-[100vh] pl-[5px] flex flex-col gap-[10px] max-[919px]:pl-[10px] max-[919px]:pt-[5px]">
-    
-        <div class="w-full flex-1 h-full rounded-[15px] bg-base-200 p-[10px]">
-            <div></div>
-            <div></div>
-        </div>
-    
-        <div class="w-full flex-1 h-full flex flex-row gap-[10px] max-[471px]:flex-col">
-    
-            <div class="flex-1 border p-[10px] rounded-[15px] bg-info">
-                Lorem ipsum dolor sit amet.
+        <div class="flex-1 flex flex-col min-h-screen">
+            <div class="flex-2 bg-base-300 p-[20px] flex flex-col justify-between items-center">
+                <nav class="px-[20px] py-[15px] w-full flex flex-row justify-between items-center"> 
+                    <aside class="p-[3px] italic"> Best of 2025</aside>
+                    <aside class="p-[3px] italic"> Modern Design</aside>
+                </nav>
+                <main class="flex-1 flex flex-col justify-center items-center">
+                    <h1 class="text-xl px-[10px] text-center">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, magnam.
+                    </h1>
+                </main>
+                <main>
+                    <p class="text-center p-[10px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid corrupti illum dignissimos odit asperiores eaque perferendis consequatur numquam cumque error.</p>
+                </main>
             </div>
-            <div class="flex-1 border p-[10px] rounded-[15px] bg-warning">
-                Lorem ipsum dolor sit amet.
+            <div class="flex-1 font-bold bg-[#c8f409] p-[20px] flex flex-col justify-center items-center"> 
+                <h1 class="text-center text-3xl line-clamp-2">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
             </div>
-    
         </div>
-    
-    </div>
-    
-    </section>
-            `
-          }
+    </section>`, 
+        },{
+            id: 'section-5_d',
+            label: 'Section - D',
+            category: 'Sections',
+            media: `
+<svg xmlns="http://www.w3.org/2000/svg" height="45" viewBox="0 -960 960 960" width="45" fill="currentColor"><path d="M200-520q-33 0-56.5-23.5T120-600v-160q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v160q0 33-23.5 56.5T760-520H200Zm0-80h560v-160H200v160Zm0 480q-33 0-56.5-23.5T120-200v-160q0-33 23.5-56.5T200-440h560q33 0 56.5 23.5T840-360v160q0 33-23.5 56.5T760-120H200Zm0-80h560v-160H200v160Zm0-400v-160 160Zm0 400v-160 160Z"/></svg>`,
+            content: `<section id="sections_041825_d" class="flex flex-rcol relative justify-center items-center min-h-[30vh] p-[20px] relative before:contentcontent-[' '] before:absolute before:top-0 before:left-0 before:bg-[#ccfe00] before:p-[20px] before:h-full before:w-[20%]">
+        <main class="min-h-screen relative z-[3] w-full flex flex-row justify-center items-center max-[836px]:flex-col">
+            <div class="flex-[1.25] h-full flex flex-col justify-center items-center"> 
+            
+                    <img src="https://images.pexels.com/photos/2678468/pexels-photo-2678468.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="shadow-xl min-h-[75vh] object-cover max-w-[320px] max-[428px]:max-w-[100%]"  alt="">
+            
+            </div>
+            <div class="flex-2 min-h-[90vh] h-full flex flex-col justify-between items-center"> 
+                 <main class="flex-1 w-full p-[20px] flex flex-col justify-center items-start">
+                    <h4 class="text-xl ">
+                        About Our Company
+                    </h4>
+                    <h1 class="text-4xl font-bold">
+                        A New Digital Marketing Base by Brand Data & Image
+                    </h1>
+                 </main>
+                 <main class="flex-1 w-full p-[20px]">
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio in hic sint quia vero quam, earum illo eaque aliquam, deserunt omnis totam corporis blanditiis, perferendis incidunt praesentium! Consectetur, maxime dolorum.
+                    </p>
+
+                    <div class="join mt-[30px]">
+                        <button class="border-0 btn btn-sm font-black bg-black text-base-300">S+</button>
+                        <button class="border-0 ml-[10px] btn btn-sm bg-[#8786d4] text-base-300 font-bold">Digital Marketing Grade</button>
+                    </div>
+                 </main>    
+            </div>
+        </main>
+    </section>`, 
+        },{
+            id: 'section-6_e',
+            label: 'Section - E',
+            category: 'Sections',
+            media: `
+<svg xmlns="http://www.w3.org/2000/svg" height="45" viewBox="0 -960 960 960" width="45" fill="currentColor"><path d="M200-520q-33 0-56.5-23.5T120-600v-160q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v160q0 33-23.5 56.5T760-520H200Zm0-80h560v-160H200v160Zm0 480q-33 0-56.5-23.5T120-200v-160q0-33 23.5-56.5T200-440h560q33 0 56.5 23.5T840-360v160q0 33-23.5 56.5T760-120H200Zm0-80h560v-160H200v160Zm0-400v-160 160Zm0 400v-160 160Z"/></svg>`,
+            content: `<section id="sections_041825_e" class="flex flex-col relative justify-center items-center min-h-[100vh] relative">
+        <div class="p-[10px]"></div>
+        <div class="flex-1 w-full flex flex-row max-[846px]:flex-col-reverse">
+            <div class="flex-1 min-h-[319.5px] relative min-w-[449.67px] max-[476px]:min-w-full max-[476px]:min-h-full">
+                <img src="https://images.pexels.com/photos/1181719/pexels-photo-1181719.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="object-cover relative w-full h-full" alt="">
+            </div>
+            <div class="flex-2 p-[20px] flex flex-col justify-center items-start px-[30px]">
+                <h4 class="font-bold italic"> The Road Map</h4>
+                <h1 class="text-4xl">
+                    Services Development
+                </h1>
+            </div>
+        </div>
+        <div class="h-[0px] flex flex-col justify-center items-center]">
+            <aside class="py-[5px] px-[15px] left-[90%] relative bg-black text-base-300 z-[9]">Learn More</aside>
+        </div>
+        <div class="flex-1 flex flex-row flex-wrap w-full bg-[#c8f409]">
+            <aside class="flex-1 border border-black/20 min-h-[298px] min-w-[300px] flex flex-col justify-center items-center p-[20px]">
+                <div class="flex flex-row justify-start items-start w-full mt-[50px] mb-[20px] iconsholder">
+                    <span class="font-black px-[15px] py-[2px] bg-[#8786d4] text-base-300 shadow-xl"> 2025 </span>
+                </div>
+                <p class="flex-1 mb-[10px]">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus similique, inventore modi ab nulla, maiores aspernatur temporibus eaque repellendus et quaerat id eligendi blanditiis, nobis perspiciatis obcaecati doloremque totam saepe!
+                </p>
+            </aside>
+            <aside class="flex-1 border border-black/20 min-h-[298px] min-w-[300px] flex flex-col justify-center items-center p-[20px]">
+                <div class="flex flex-row justify-start items-start w-full mt-[50px] mb-[20px] iconsholder">
+                    <span class="font-black px-[15px] py-[2px] bg-[#8786d4] text-base-300 shadow-xl"> 2025 </span>
+                </div>
+                <p class="flex-1 mb-[10px]">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus similique, inventore modi ab nulla, maiores aspernatur temporibus eaque repellendus et quaerat id eligendi blanditiis, nobis perspiciatis obcaecati doloremque totam saepe!
+                </p>
+            </aside>
+            <aside class="flex-1 border border-black/20 min-h-[298px] min-w-[300px] flex flex-col justify-center items-center p-[20px]">
+                <div class="flex flex-row justify-start items-start w-full mt-[50px] mb-[20px] iconsholder">
+                    <span class="font-black px-[15px] py-[2px] bg-[#8786d4] text-base-300 shadow-xl"> 2025 </span>
+                </div>
+                <p class="flex-1 mb-[10px]">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi dicta eveniet dolore quidem laboriosam, nisi sapiente corporis facere impedit nulla aut qui architecto rerum assumenda eius, error accusamus autem ratione nesciunt fugit suscipit dignissimos quis. Rem, vero? Vel, similique iusto.
+                </p>
+            </aside>
+        </div>
+    </section>`, 
+        },
     ]
 }
+
+
+/*
+{
+    id: 'section-3_a',
+    label: 'Section - B',
+    category: 'Sections',
+            media: `
+<svg xmlns="http://www.w3.org/2000/svg" height="45" viewBox="0 -960 960 960" width="45" fill="currentColor"><path d="M200-520q-33 0-56.5-23.5T120-600v-160q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v160q0 33-23.5 56.5T760-520H200Zm0-80h560v-160H200v160Zm0 480q-33 0-56.5-23.5T120-200v-160q0-33 23.5-56.5T200-440h560q33 0 56.5 23.5T840-360v160q0 33-23.5 56.5T760-120H200Zm0-80h560v-160H200v160Zm0-400v-160 160Zm0 400v-160 160Z"/></svg>`,
+    content: ` `
+},
+*/ 
 
