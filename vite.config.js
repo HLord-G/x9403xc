20807d32b5f0d5bdd52794c7234f487d0a06774d
Harold Guzman <Gzmn.h10195@gmail.com>
2025-04-23 22:57:17 +0800

    de

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: './', // optional, useful kung static deployment like Vercel or GitHub Pages\
+  // base: './', // optional, useful kung static deployment like Vercel or GitHub Pages\
   plugins: [
     tailwindcss()
   ],
