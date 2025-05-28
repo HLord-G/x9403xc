8338c0834c3f47a5ff1642fd0e28e21de14886b9
Harold Guzman <Gzmn.h10195@gmail.com>
2025-05-28 21:34:49 +0800

    gggg

--- src/app.js
+++ src/app.js
@@ -30,145 +30,149 @@ document.getElementById("popupclosediag").addEventListener("click", () => {
 
 let scriptHolder = "script"
 
-
-GrapesJsStudioSDK.createStudioEditor({
-  root: '#studio',
-  blocks: {
-  default: _xx947xfee_()
-  },
-
-  layout: {
-    default: {
-      type: 'row',
-      style: { height: '100%' },
-      children: [
-        {
-          type: 'sidebarLeft',
-          children: {
-            type: 'tabs',
-            value: 'blocks',
-            tabs: [
-              {
-                id: 'pages',
-                label: '🗃️',
-                children: {    
-                media: '<svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#e8eaed"><path d="M560-440h200v-80H560v80Zm0-120h200v-80H560v80ZM200-320h320v-22q0-45-44-71.5T360-440q-72 0-116 26.5T200-342v22Zm160-160q33 0 56.5-23.5T440-560q0-33-23.5-56.5T360-640q-33 0-56.5 23.5T280-560q0 33 23.5 56.5T360-480ZM160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm0-80h640v-480H160v480Zm0 0v-480 480Z"/></svg>',
-
-                  type: 'panelPages', style: { height: '100%' } },
-              },
-              {
-                id: 'layers',
-                label: '⚙️',
-                children: { type: 'panelLayers', style: { height: '100%' } },
-              },
-              {
-                id: 'blocks',
-                label: '🧩',
-                children: { type: 'panelBlocks', style: { height: '100%' } },
-              },
-              {
-                id: 'template',
-                label: '🎨',
+try {
+  GrapesJsStudioSDK.createStudioEditor({
+    root: '#studio',
+    blocks: {
+    default: _xx947xfee_()
+    },
+  
+    layout: {
+      default: {
+        type: 'row',
+        style: { height: '100%' },
+        children: [
+          {
+            type: 'sidebarLeft',
+            children: {
+              type: 'tabs',
+              value: 'blocks',
+              tabs: [
+                {
+                  id: 'pages',
+                  label: '🗃️',
+                  children: {    
+                  media: '<svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#e8eaed"><path d="M560-440h200v-80H560v80Zm0-120h200v-80H560v80ZM200-320h320v-22q0-45-44-71.5T360-440q-72 0-116 26.5T200-342v22Zm160-160q33 0 56.5-23.5T440-560q0-33-23.5-56.5T360-640q-33 0-56.5 23.5T280-560q0 33 23.5 56.5T360-480ZM160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm0-80h640v-480H160v480Zm0 0v-480 480Z"/></svg>',
+  
+                    type: 'panelPages', style: { height: '100%' } },
+                },
+                {
+                  id: 'layers',
+                  label: '⚙️',
+                  children: { type: 'panelLayers', style: { height: '100%' } },
+                },
+                {
+                  id: 'blocks',
+                  label: '🧩',
+                  children: { type: 'panelBlocks', style: { height: '100%' } },
+                },
+                {
+                  id: 'template',
+                  label: '🎨',
+                  
+                  children: { type: 'panelTemplates', style: { height: '100%', padding:"20px 5px" },
+                  content: { itemsPerRow: 1 },
+                  },
+                },
                 
-                children: { type: 'panelTemplates', style: { height: '100%', padding:"20px 5px" },
-                content: { itemsPerRow: 1 },
-                },
-              },
-              
-
-              // panelTemplates
-            ],
+  
+                // panelTemplates
+              ],
+            },
           },
-        },
-        {
-          type: 'canvasSidebarTop',
-          sidebarTop: { leftContainer: { buttons: [] } },
-        },
-        {
-          type: 'sidebarRight',
-          children: {
-            type: 'tabs',
-            value: 'styles',
-            tabs: [
-              {
-                id: 'styles',
-                label: 'Styles',
-                children: {
-                  type: 'column',
-                  style: { height: '100%' },
-                  children: [
-                    { type: 'panelSelectors', style: { padding: 5 } },
-                    { type: 'panelStyles' },
-                  ],
-                },
-              },
-              {
-                id: 'props',
-                label: 'Properties',
-                children: { type: 'panelProperties', style: { padding: 5, height: '100%' } },
-              },
-              {
-                id: 'custom',
-                label: 'Custom',
-                children: ['New custom tab'],
-              },
-            ],
+          {
+            type: 'canvasSidebarTop',
+            sidebarTop: { leftContainer: { buttons: [] } },
           },
-        },
-      ],
-    },
-},
-
-
-
-
-templates: {
-  layout: {
-    type: 'panelTemplates',
-    content: { itemsPerRow: 2 },
-
+          {
+            type: 'sidebarRight',
+            children: {
+              type: 'tabs',
+              value: 'styles',
+              tabs: [
+                {
+                  id: 'styles',
+                  label: 'Styles',
+                  children: {
+                    type: 'column',
+                    style: { height: '100%' },
+                    children: [
+                      { type: 'panelSelectors', style: { padding: 5 } },
+                      { type: 'panelStyles' },
+                    ],
+                  },
+                },
+                {
+                  id: 'props',
+                  label: 'Properties',
+                  children: { type: 'panelProperties', style: { padding: 5, height: '100%' } },
+                },
+                {
+                  id: 'custom',
+                  label: 'Custom',
+                  children: ['New custom tab'],
+                },
+              ],
+            },
+          },
+        ],
+      },
   },
-  // The onLoad can be an asyncronous function, so you can fetch templates from your API
-  onLoad: async () => x_fu33fd_x()
-},
-
-
-
-  project: {
-    type: 'web',
-
-    default: {
-      custom: {
-          globalPageSettings: {
-            generator:"FlexBoy Studio",
-            title: 'FlexBoy Web Design',
-            description: 'FlexBoy description',
-            customCodeHead: `
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
-    <${scriptHolder} src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></${scriptHolder}>
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
-    <${scriptHolder} src="https://code.jquery.com/jquery-3.7.1.js"></${scriptHolder}>
-    <${scriptHolder} src="https://code.jquery.com/ui/1.14.1/jquery-ui.js"></${scriptHolder}>
-    <${scriptHolder} src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"></${scriptHolder}>
-    <${scriptHolder} src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js"></${scriptHolder}>
-            `,
-            // customCodeBody: '<div>de</div>',
-          }
-        },
-
-
-      pages: [
-        {
-          name: 'index',
-          component: `<h1 class="p-[2rem] text-center">
-            Hello Welcome to FlexBoy Web Design 👋 
-          </h1>`,
-          styles: '#comp3 { color: blue }',
-        } 
-      ],
+  
+  
+  
+  
+  templates: {
+    layout: {
+      type: 'panelTemplates',
+      content: { itemsPerRow: 2 },
+  
     },
-  }
-});
+    // The onLoad can be an asyncronous function, so you can fetch templates from your API
+    onLoad: async () => x_fu33fd_x()
+  },
+  
+  
+  
+    project: {
+      type: 'web',
+  
+      default: {
+        custom: {
+            globalPageSettings: {
+              generator:"FlexBoy Studio",
+              title: 'FlexBoy Web Design',
+              description: 'FlexBoy description',
+              customCodeHead: `
+      <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+      <${scriptHolder} src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></${scriptHolder}>
+      <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
+      <${scriptHolder} src="https://code.jquery.com/jquery-3.7.1.js"></${scriptHolder}>
+      <${scriptHolder} src="https://code.jquery.com/ui/1.14.1/jquery-ui.js"></${scriptHolder}>
+      <${scriptHolder} src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"></${scriptHolder}>
+      <${scriptHolder} src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js"></${scriptHolder}>
+              `,
+              // customCodeBody: '<div>de</div>',
+            }
+          },
+  
+  
+        pages: [
+          {
+            name: 'index',
+            component: `<h1 class="p-[2rem] text-center">
+              Hello Welcome to FlexBoy Web Design 👋 
+            </h1>`,
+            styles: '#comp3 { color: blue }',
+          } 
+        ],
+      },
+    }
+  });
+} catch (error) {
+  location.reload();
+}
+
 
  
 
